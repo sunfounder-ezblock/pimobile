fb7303b199d42037c95544acea7c89aaa063547b
Cavon <cavonxx@gmail.com>
2019-01-18 10:46:03 +0800

    fix msg files

--- msg/en.js
+++ msg/en.js
@@ -1,39 +1,33 @@
-'use strict';
+Local['en'].MSG.catPiMobile = "PiMobile";
 
-goog.provide('Blockly.Msg.pimobile.en');
-
-goog.require('Blockly.Msg');
-
-MSG.catPiMobile = "PiMobile";
-
-MSG.pimobile_description = "Raspberry Pi Mobile Car"
+Local['en'].MSG.pimobile_description = "Raspberry Pi Mobile Car"
 
 
-Blockly.Msg.PIMOBILE_DIGITAL_PIN_TITLE = "D"
-Blockly.Msg.PIMOBILE_DIGITAL_PIN_TOOLTIP = "Digital pin number";
+Local['en'].PIMOBILE_DIGITAL_PIN_TITLE = "D"
+Local['en'].PIMOBILE_DIGITAL_PIN_TOOLTIP = "Digital pin number";
 
-Blockly.Msg.PIMOBILE_ANALOG_PIN_TITLE = "A"
-Blockly.Msg.PIMOBILE_ANALOG_PIN_TOOLTIP = "Analog pin number";
+Local['en'].PIMOBILE_ANALOG_PIN_TITLE = "A"
+Local['en'].PIMOBILE_ANALOG_PIN_TOOLTIP = "Analog pin number";
 
-Blockly.Msg.PIMOBILE_PWM_PIN_TITLE = "PWM"
-Blockly.Msg.PIMOBILE_PWM_PIN_TOOLTIP = "PWM pin number";
+Local['en'].PIMOBILE_PWM_PIN_TITLE = "PWM"
+Local['en'].PIMOBILE_PWM_PIN_TOOLTIP = "PWM pin number";
 
-Blockly.Msg.PIMOBILE_DEVICE_PIN_TOOLTIP = "Device pin number";
+Local['en'].PIMOBILE_DEVICE_PIN_TOOLTIP = "Device pin number";
 
 
-Blockly.Msg.PIMOBILE_LINE_TOOLTIP = "Select line follower";
+Local['en'].PIMOBILE_LINE_TOOLTIP = "Select line follower";
 
-Blockly.Msg.PIMOBILE_MOTOR_SELECT_TOOLTIP = "Select Motor ";
+Local['en'].PIMOBILE_MOTOR_SELECT_TOOLTIP = "Select Motor ";
 
-Blockly.Msg.PIMOBILE_LINE_FOLLOWER_TITLE1 = "Get line follower"
-Blockly.Msg.PIMOBILE_LINE_FOLLOWER_TITLE2 = "value"
-Blockly.Msg.PIMOBILE_LINE_FOLLOWER_TOOLTIP = "return the value of the line follower";
+Local['en'].PIMOBILE_LINE_FOLLOWER_TITLE1 = "Get line follower"
+Local['en'].PIMOBILE_LINE_FOLLOWER_TITLE2 = "value"
+Local['en'].PIMOBILE_LINE_FOLLOWER_TOOLTIP = "return the value of the line follower";
 
-Blockly.Msg.PIMOBILE_ULTARSONIC_DISTANCE_TITLE1 = "Get ultarsonic value";
-Blockly.Msg.PIMOBILE_ULTARSONIC_DISTANCE_TITLE2 = "Triq";
-Blockly.Msg.PIMOBILE_ULTARSONIC_DISTANCE_TITLE3 = "Echo";
-Blockly.Msg.PIMOBILE_ULTARSONIC_TOOLTIP = "Get ultarsonic value";
+Local['en'].PIMOBILE_ULTARSONIC_DISTANCE_TITLE1 = "Get ultarsonic value";
+Local['en'].PIMOBILE_ULTARSONIC_DISTANCE_TITLE2 = "Triq";
+Local['en'].PIMOBILE_ULTARSONIC_DISTANCE_TITLE3 = "Echo";
+Local['en'].PIMOBILE_ULTARSONIC_TOOLTIP = "Get ultarsonic value";
 
-Blockly.Msg.PIMOBILE_MOTOR_TITLE1 = "Set motor";
-Blockly.Msg.PIMOBILE_MOTOR_TITLE2 = "value to";
-Blockly.Msg.PIMOBILE_MOTOR_TOOLTIP = "Set motor pulse width value (-4095 ~ 4095). Negative values represent backwards, positive values represent foewards. ";
+Local['en'].PIMOBILE_MOTOR_TITLE1 = "Set motor";
+Local['en'].PIMOBILE_MOTOR_TITLE2 = "value to";
+Local['en'].PIMOBILE_MOTOR_TOOLTIP = "Set motor pulse width value (-4095 ~ 4095). Negative values represent backwards, positive values represent foewards. ";
--- msg/zh-hans.js
+++ msg/zh-hans.js
@@ -1,39 +1,33 @@
-'use strict';
+Local['zh-hans'].MSG.catPiMobile = "PiMobile";
 
-goog.provide('Blockly.Msg.pimobile.cn');
-
-goog.require('Blockly.Msg');
-
-MSG.catPiMobile = "PiMobile";
-
-MSG.pimobile_description = "树莓派新一代小车"
+Local['zh-hans'].MSG.pimobile_description = "树莓派新一代小车"
 
 
-Blockly.Msg.PIMOBILE_DIGITAL_PIN_TITLE = "D"
-Blockly.Msg.PIMOBILE_DIGITAL_PIN_TOOLTIP = "数字引脚";
+Local['zh-hans'].PIMOBILE_DIGITAL_PIN_TITLE = "D"
+Local['zh-hans'].PIMOBILE_DIGITAL_PIN_TOOLTIP = "数字引脚";
 
-Blockly.Msg.PIMOBILE_ANALOG_PIN_TITLE = "A"
-Blockly.Msg.PIMOBILE_ANALOG_PIN_TOOLTIP = "模拟引脚";
+Local['zh-hans'].PIMOBILE_ANALOG_PIN_TITLE = "A"
+Local['zh-hans'].PIMOBILE_ANALOG_PIN_TOOLTIP = "模拟引脚";
 
-Blockly.Msg.PIMOBILE_PWM_PIN_TITLE = "PWM"
-Blockly.Msg.PIMOBILE_PWM_PIN_TOOLTIP = "PWM 引脚";
+Local['zh-hans'].PIMOBILE_PWM_PIN_TITLE = "PWM"
+Local['zh-hans'].PIMOBILE_PWM_PIN_TOOLTIP = "PWM 引脚";
 
-Blockly.Msg.PIMOBILE_DEVICE_PIN_TOOLTIP = "内置硬件引脚";
+Local['zh-hans'].PIMOBILE_DEVICE_PIN_TOOLTIP = "内置硬件引脚";
 
 
-Blockly.Msg.PIMOBILE_LINE_TOOLTIP = "选择巡线";
+Local['zh-hans'].PIMOBILE_LINE_TOOLTIP = "选择巡线";
 
-Blockly.Msg.PIMOBILE_MOTOR_SELECT_TOOLTIP = "选择电机";
+Local['zh-hans'].PIMOBILE_MOTOR_SELECT_TOOLTIP = "选择电机";
 
-Blockly.Msg.PIMOBILE_LINE_FOLLOWER_TITLE1 = "获取巡线"
-Blockly.Msg.PIMOBILE_LINE_FOLLOWER_TITLE2 = "的值"
-Blockly.Msg.PIMOBILE_LINE_FOLLOWER_TOOLTIP = "返回巡线的值";
+Local['zh-hans'].PIMOBILE_LINE_FOLLOWER_TITLE1 = "获取巡线"
+Local['zh-hans'].PIMOBILE_LINE_FOLLOWER_TITLE2 = "的值"
+Local['zh-hans'].PIMOBILE_LINE_FOLLOWER_TOOLTIP = "返回巡线的值";
 
-Blockly.Msg.PIMOBILE_ULTARSONIC_DISTANCE_TITLE1 = "获取超声波的值";
-Blockly.Msg.PIMOBILE_ULTARSONIC_DISTANCE_TITLE2 = "Triq";
-Blockly.Msg.PIMOBILE_ULTARSONIC_DISTANCE_TITLE3 = "Echo";
-Blockly.Msg.PIMOBILE_ULTARSONIC_TOOLTIP = "获取超声波的值";
+Local['zh-hans'].PIMOBILE_ULTARSONIC_DISTANCE_TITLE1 = "获取超声波的值";
+Local['zh-hans'].PIMOBILE_ULTARSONIC_DISTANCE_TITLE2 = "Triq";
+Local['zh-hans'].PIMOBILE_ULTARSONIC_DISTANCE_TITLE3 = "Echo";
+Local['zh-hans'].PIMOBILE_ULTARSONIC_TOOLTIP = "获取超声波的值";
 
-Blockly.Msg.PIMOBILE_MOTOR_TITLE1 = "设置电机";
-Blockly.Msg.PIMOBILE_MOTOR_TITLE2 = "的值为";
-Blockly.Msg.PIMOBILE_MOTOR_TOOLTIP = "设置电机的值 (-4095 ~ 4095).负值代表后退，正值代表前进. ";
+Local['zh-hans'].PIMOBILE_MOTOR_TITLE1 = "设置电机";
+Local['zh-hans'].PIMOBILE_MOTOR_TITLE2 = "的值为";
+Local['zh-hans'].PIMOBILE_MOTOR_TOOLTIP = "设置电机的值 (-4095 ~ 4095).负值代表后退，正值代表前进. ";
